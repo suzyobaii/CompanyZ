7076831a0ecf49dd6a307ae8e40149c938d13804
Tina Tran <trantina574@gmail.com>
2025-12-01 13:43:05 -0500

    Starting maven project

---
+++ ems-app/src/main/java/corelink/App.java
@@ -0,0 +1,13 @@
+package corelink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
